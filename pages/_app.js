6321157eb96abe5d8cb45dca217cb4674d7d314f
manast23 <muhammadanast07@gmail.com>
2026-09-16 10:45:04 +0500

    Wire up Vercel Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/next'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
